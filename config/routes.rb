1f00eea306cd8b67d1f72d20e44bb21298b2c738
Mohamed Shahul Hameed <thameemsh@email.com>
2022-01-12 11:30:27 -0500

    Add controller to show login form, authenticate and logout

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create' 
 
